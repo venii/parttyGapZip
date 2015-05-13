020a5865336d62c02364338dcd726c2d7b9ec8ec
vinicius~~ <viniciusferreirwk@gmail.com>
2015-05-13 14:28:13 -0300

    add toolbar=no -> 4974

--- www/lib/ngCordova/dist/ng-cordova.js
+++ www/lib/ngCordova/dist/ng-cordova.js
@@ -4962,7 +4962,7 @@ angular.module("ngCordova.plugins.oauth", ["ngCordova.plugins.oauthUtility"])
             if(window.cordova) {
                 var cordovaMetadata = cordova.require("cordova/plugin_list").metadata;
                 if(cordovaMetadata.hasOwnProperty("org.apache.cordova.inappbrowser") === true) {
-                    var browserRef = window.open('https://www.facebook.com/dialog/oauth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&response_type=token&scope=' + appScope.join(","), '_blank', 'location=no,clearsessioncache=yes,clearcache=yes');
+                    var browserRef = window.open('https://www.facebook.com/dialog/oauth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&response_type=token&scope=' + appScope.join(","), '_blank', 'location=no,toolbar=no,clearsessioncache=yes,clearcache=yes');
                     browserRef.addEventListener('loadstart', function(event) {
                         if((event.url).indexOf("http://localhost/callback") === 0) {
                             var callbackResponse = (event.url).split("#")[1];
