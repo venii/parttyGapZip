46dd82df02f1fe3aab176d08bc7ba0a2a756f945
vinicius~~ <viniciusferreirwk@gmail.com>
2015-08-15 19:05:17 -0300

    sqlite created

--- www/js/controllers/chat-controllers.js
+++ www/js/controllers/chat-controllers.js
@@ -31,6 +31,42 @@ angular.module('chat.controllers', ['starter'])
 
   	};
 }).service('ChatMessageService',function($sce,$compile,$localStorage,$ionicViewService,$http,$rootScope,$state,$ionicLoading,$templateRequest,$ionicSideMenuDelegate) {
+           
+           this.initDB = function(){
+                try{
+                  var db = window.sqlitePlugin.openDatabase({name: "dbapp_partty.db"});
+
+                  db.transaction(function(tx) {
+                    //tx.executeSql('DROP TABLE IF EXISTS test_table');
+                    tx.executeSql('CREATE TABLE IF NOT EXISTS chat (id integer primary key, data text, data_num integer)');
+
+                    
+
+                    // demonstrate PRAGMA:
+                    /*db.executeSql("pragma table_info (test_table);", [], function(res) {
+                      console.log("PRAGMA res: " + JSON.stringify(res));
+                    });
+
+                    tx.executeSql("INSERT INTO test_table (data, data_num) VALUES (?,?)", ["test", 100], function(tx, res) {
+                      console.log("insertId: " + res.insertId + " -- probably 1");
+                      console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+
+                      db.transaction(function(tx) {
+                        tx.executeSql("select count(id) as cnt from test_table;", [], function(tx, res) {
+                          console.log("res.rows.length: " + res.rows.length + " -- should be 1");
+                          console.log("res.rows.item(0).cnt: " + res.rows.item(0).cnt + " -- should be 1");
+                        });
+                      });
+
+                    }, function(e) {
+                      console.log("ERROR: " + e.message);
+                    });*/
+                  });
+                }catch(err){
+                    alert("ERROR DB LOAD");
+                }
+           }
+
            this.sendMessageWS = function($scope,msg,idtosend){
            		
            		var postData = {
@@ -57,6 +93,9 @@ angular.module('chat.controllers', ['starter'])
            this.addMSGtoList = function(msg){
                 if($rootScope.logset == undefined)
                   $rootScope.logset = new Array();
+
+
+                this.initDB();
                 
                 $rootScope.logset.push(msg);
            }
