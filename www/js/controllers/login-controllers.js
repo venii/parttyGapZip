f29d6e7209435b3360beea44e64ece7a4ab18a84
vinicius~~ <viniciusferreirwk@gmail.com>
2015-09-07 10:21:59 -0300

    fix login layout

--- www/js/controllers/login-controllers.js
+++ www/js/controllers/login-controllers.js
@@ -130,4 +130,23 @@ angular.module('login.controllers', ['starter'])
   },1000);
  
    
+}).controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 5000;
+  $scope.noWrapSlides = false;
+  var slides = $scope.slides = [];
+  slides.push({image : 'img/gal/gal_img1.jpg' , text : ''});
+  slides.push({image : 'img/gal/gal_img2.jpg' , text : ''});
+  slides.push({image : 'img/gal/gal_img3.jpg' , text : ''});
+  /*
+  $scope.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: '//placekitten.com/' + newWidth + '/300',
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }*/
 });
