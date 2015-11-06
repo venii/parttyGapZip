863cf2b7e793e7b2a6c9aeb4cf06b668ad2be9dc
veni <viniciusferreirawk@gmail.com>
2015-11-06 12:16:23 -0200

    fix matches partty

--- www/js/controllers/cards-animation-matches-controllers.js
+++ www/js/controllers/cards-animation-matches-controllers.js
@@ -7,73 +7,41 @@ angular.module('cards-animation-matches.controllers', ['starter', 'gajus.swing',
                 RegistrationService,MenuService) {
         
         MenuService.blockSideMenu(); 
+      
+        var cards = SendMatchesToWS.getCards();
+        $scope.cards = cards;
+        $scope.cardrest = cards.length;
         $scope.eventinfoJSON = $stateParams.dataEvent;
 
-        
-        
-        $scope.cards = SendMatchesToWS.getCards();
-        $scope.cardrest = $scope.cards.length;
-
-
-
-        console.log('send',$scope);
-        
-        if(SendMatchesToWS.getCards().length > 0){
+        $scope.clickLeft = function(){       
+            index = ($scope.cardrest);
             
-            angular.forEach($rootScope.matchesData.data, function(value, key) {
-                if(value.id != $localStorage.usuarioData.ent_fbid)
-                    $scope.cards.push(value);
-            });
-
-        }
-        
-
-        /*
-        $scope.cardrest = $rootScope.matchesData.data.length-1;
-        
-        $scope.clickLeft = function(){
+            $scope.cards[index].animateclass = 'moveleft';
+            $scope.cards[index].actionclick = 1;
+            $scope.cards[index].ent_fbid = RegistrationService.getUserFbID();
+            $scope.cards[index].ent_id_event = '';
             
-           
-                    index = ($scope.cardrest);
-                    console.log(index);
-                    $scope.cards[index].animateclass = 'moveleft';
-                    $scope.cards[index].actionclick = 1;
-                    $scope.cards[index].ent_fbid = RegistrationService.getUserFbID();
-                    $scope.cards[index].ent_id_event = '';
-                    
-                    //$scope.cards.splice(index, 1);
-                    SendMatchesToWS.sendInvite($scope.cards[index],function(resp){
-                        console.log('Chegou',resp);
-                        if($scope.cardrest < 0){
-                            SendMatchesToWS.sendMatches($scope.cards,$scope);
-                        }
-                     });
-                    $scope.cardrest--;
-            
-               
+            SendMatchesToWS.sendInvite($scope.cards[index],function(resp){
+                if($scope.cardrest < 0){
+                    SendMatchesToWS.sendMatches($scope.cards,$scope);
+                }
+             });
+            $scope.cardrest--;
         }
 
-        $scope.clickRight = function(){
-            
-                     index = ($scope.cardrest);
-                     
-                     $scope.cards[index].animateclass = 'moveright';
-                     $scope.cards[index].actionclick = 2;
-                     $scope.cards.splice(index, 1);  
-                     $scope.cardrest--;
+        $scope.clickRight = function(){    
+            index = ($scope.cardrest);
+         
+            $scope.cards[index].animateclass = 'moveright';
+            $scope.cards[index].actionclick = 2;
+            $scope.cards.splice(index, 1);  
+            $scope.cardrest--;
 
-                     SendMatchesToWS.sendInvite($scope.cards[index],function(resp){
-                        console.log('Chegou',resp);
-                        if($scope.cardrest < 0){
-                            SendMatchesToWS.sendMatches($scope.cards,$scope);
-                         }    
-                     });
-
-                     
-                  
+            SendMatchesToWS.sendInvite($scope.cards[index],function(resp){
+               if($scope.cardrest < 0){
+                  SendMatchesToWS.sendMatches($scope.cards,$scope);
+               }    
+            });                  
         }
 
-        */
-        
-
-    })
+})
