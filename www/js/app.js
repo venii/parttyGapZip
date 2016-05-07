cfe0df8f25fc253d21f97f387e31bcf215dfc25a
veni <viniciusferreirawk@gmail.com>
2016-05-07 14:12:48 -0300

    Comentarios

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,14 @@
-angular.module('starter', ['ionic','ui.bootstrap',
+angular.module('starter', [	 'ionic',
+							 'ui.bootstrap',
                              'openfb',
-                             'ionic.service.core','ionic.service.push',
-                             'ng','ngCordova','ngLoad','ngStorage',
+                             'ionic.service.core',
+                             'ionic.service.push',
+                             'ng',
+                             'ngCordova',
+                             'ngLoad',
+                             'ngStorage',
                              'ngIOS9UIWebViewPatch',
-                              
+							 /*CONTROLLERS*/                              
                              'app.menu',
                              'login.controllers',
                              'main.controllers',
@@ -26,32 +31,16 @@ angular.module('starter', ['ionic','ui.bootstrap',
                              'app.event-service',
                              'app.match-service',
 
-                                       ])
-
+  ])
   .run(function($ionicPlatform,OpenFB) {
 
     $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
- 
-    	/*
-      if (window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
-        StatusBar.styleDefault();
-      }
-		*/
    
     });
 
-    /*teste iaia*/ 
   })
-
   .config(function($stateProvider, $urlRouterProvider,$httpProvider) {
-
-
+  	//Routes do app
     $stateProvider
 
     .state('app', {
@@ -60,160 +49,129 @@ angular.module('starter', ['ionic','ui.bootstrap',
       templateUrl: "templates/core/menu.html",
       controller: 'AppCtrl'
     })
-
-
-	    .state('app.loggedout', {
-	      url: "/loggedout",
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/login/loggedout.html",
-	          controller: 'LoggedOutCtrl'
-	        }
-	      }
-	    })
-	    .state('app.chat', {
-	      url: "/chat/:idfb",
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/chat/chat.html",
-	           controller: 'ChatCtrl'
-	        }
-	      }
-	    })
-
-	    .state('app.main', {
-	      url: "/main",
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/core/main.html",
-	          controller: 'MainCtrl'
-	        }
-	      },
-	      
-	    })
-	    
-	    .state('app.registration', {
-	      url: "/registration",
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/core/registration.html",
-	          controller: 'RegistrationCtrl'
-	        }
-	      },
-	      
-	    })
-
-	      .state('app.login', {
-	        url: "/login",
-	        views: {
-	          'menuContent': {
-	            templateUrl: "templates/login/loginfb.html",
-	            controller: 'LoginFBCtrl'
-	          }
-	        }
-	      })
-
-	    .state('app.configurations', {
-	      url: "/configurations",
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/configurations/configurations.html",
-	          controller: 'ConfigurationsCtrl'
-	        }
-	      },
-	      
-	    }).state('app.events', {
-	      url: "/events",
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/events/events.html",
-	          controller: 'EventsCtrl'
-	        }
-	      },
-	      //remover de app para nao carregar o side menu =)
-	    }).state('app.profile', {
-	      url: "/profile",
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/profile/profile.html",
-	          controller: 'ProfileCtrl'
-	        }
-	      },
-	      //remover de app para nao carregar o side menu =)
-	    }).state('app.matches', {
-	      url: "/matches",
-	      params : { dataEvent: null },      
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/matches/matches.html",
-	          controller: 'MatchesCtrl'
-	        }
-	      },onExit: function(MatchService){
-	          MatchService.showTopMenu();
-	      }
-	      
-	    }).state('app.newmatchesfound', {
-	      url: "/newmatchesfound",
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/newmatchesfound/newmatchesfound.html",
-	          controller: 'NewMatchesFoundCtrl'
-	        }
-	      },onExit: function(){
-	            angular.element(document.querySelector('#menuAPP')).removeClass('hidden');
-	          
-	      },onEnter: function(){
-
-	            angular.element(document.querySelector('#menuAPP')).addClass('hidden');
-	            //angular.element(document.querySelector('#matchesView')).addClass('menuUptoTop');
-
-
-	            angular.element(document.querySelector('#newmatchesfoundView')).css("margin-top", "-40px");
-	            
-	            //angular.element(document.querySelector('#matchesView')).css("margin-top", "-40px");
-	            //alert("@#");
-	      }
-	      
-	    }).state('app.newmatchesfound_receive', {
-	      url: "/newmatchesfound_receive",
-	      views: {
-	        'menuContent': {
-	          templateUrl: "templates/newmatchesfound/newmatchesfound.html",
-	          controller: 'NewMatchesFoundReceiveCtrl'
-	        }
-	      },onExit: function(){
-	            angular.element(document.querySelector('#menuAPP')).removeClass('hidden');
-	          
-	      },onEnter: function(){
-
-	            angular.element(document.querySelector('#menuAPP')).addClass('hidden');
-	            //angular.element(document.querySelector('#matchesView')).addClass('menuUptoTop');
-
-
-	            angular.element(document.querySelector('#newmatchesfoundView')).css("margin-top", "-40px");
-	            
-	            //angular.element(document.querySelector('#matchesView')).css("margin-top", "-40px");
-	            //alert("@#");
-	      }
-	      
-	    });
-    // if none of the above states are matched, use this as the fallback
+    .state('app.loggedout', {
+      url: "/loggedout",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/login/loggedout.html",
+          controller: 'LoggedOutCtrl'
+        }
+      }
+    })
+    .state('app.chat', {
+      url: "/chat/:idfb",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/chat/chat.html",
+           controller: 'ChatCtrl'
+        }
+      }
+    })
+    .state('app.main', {
+      url: "/main",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/core/main.html",
+          controller: 'MainCtrl'
+        }
+      }
+      
+    })
+    .state('app.registration', {
+      url: "/registration",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/core/registration.html",
+          controller: 'RegistrationCtrl'
+        }
+      } 
+    })
+    .state('app.login', {
+        url: "/login",
+        views: {
+          'menuContent': {
+            templateUrl: "templates/login/loginfb.html",
+            controller: 'LoginFBCtrl'
+          }
+        }
+    })
+    .state('app.configurations', {
+      url: "/configurations",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/configurations/configurations.html",
+          controller: 'ConfigurationsCtrl'
+        }
+      },
+      
+    }).state('app.events', {
+      url: "/events",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/events/events.html",
+          controller: 'EventsCtrl'
+        }
+      },
+    }).state('app.profile', {
+      url: "/profile",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/profile/profile.html",
+          controller: 'ProfileCtrl'
+        }
+      },
+    }).state('app.matches', {
+      url: "/matches",
+      params : { dataEvent: null },      
+      views: {
+        'menuContent': {
+          templateUrl: "templates/matches/matches.html",
+          controller: 'MatchesCtrl'
+        }
+      },onExit: function(MatchService){
+      	  //mostra menu do topo
+          MatchService.showTopMenu();
+      }    
+    }).state('app.newmatchesfound', {
+      url: "/newmatchesfound",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/newmatchesfound/newmatchesfound.html",
+          controller: 'NewMatchesFoundCtrl'
+        }
+      },onExit: function(){
+      		//Ajuste de CSS
+            angular.element(document.querySelector('#menuAPP')).removeClass('hidden');   
+      },onEnter: function(){
+      		//Ajuste de CSS
+            angular.element(document.querySelector('#menuAPP')).addClass('hidden');
+            angular.element(document.querySelector('#newmatchesfoundView')).css("margin-top", "-40px");
+      }
+    }).state('app.newmatchesfound_receive', {
+      url: "/newmatchesfound_receive",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/newmatchesfound/newmatchesfound.html",
+          controller: 'NewMatchesFoundReceiveCtrl'
+        }
+      },onExit: function(){
+      		//Ajuste de CSS
+            angular.element(document.querySelector('#menuAPP')).removeClass('hidden');   
+      },onEnter: function(){
+      		//Ajuste de CSS
+            angular.element(document.querySelector('#menuAPP')).addClass('hidden');
+            angular.element(document.querySelector('#newmatchesfoundView')).css("margin-top", "-40px");
+      }
+      
+    });
+    //Se não tiver autenticado vai para o login
     $urlRouterProvider.otherwise('/app/login');
 
-
-    
-  }).config(['$ionicAppProvider', function($ionicAppProvider) {
-    // Identify app
-    $ionicAppProvider.identify({
-      // The App ID (from apps.ionic.io) for the server
-      app_id: '93517726',
-      // The public API key all services will use for this app
-      api_key: '55d0b73b57dbec9303196edb48750c9ffd85236e5ed35f12'
-    });
-  }]).config(['$httpProvider', function($httpProvider) {
-    // Identify app
+  })
+  .config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
-     delete $httpProvider.defaults.headers.common['X-Requested-With'];
-  }]).config(['$compileProvider', function($compileProvider) {
-            $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|file|blob|content):|data:image\//);
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  }])
+  .config(['$compileProvider', function($compileProvider) {
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|file|blob|content):|data:image\//);
   }]);
