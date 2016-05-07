48e6c13cfb768c5d72e334b4d4075ea321b5598a
veni <viniciusferreirawk@gmail.com>
2016-05-07 16:11:15 -0300

    Comentarios

--- www/js/modal/modal-controllers.js
+++ www/js/modal/modal-controllers.js
@@ -2,30 +2,24 @@ angular.module('modal.controllers', ['starter'])
 
 .controller('ModalDobCtrl', function ($scope, $modalInstance,$localStorage,$http,$state) {
 
-  //$scope.items = items;
-  //$scope.selected = {
-  //  item: $scope.items[0]
- // };
-
   $scope.ok = function () {
     var date = $scope.dt;
     var dd = date.getDate();
-	var mm = date.getMonth()+1; //January is 0!
+	  var mm = date.getMonth()+1; //January is 0!
 
-	var yyyy = date.getFullYear();
+	  var yyyy = date.getFullYear();
 	  
-	var postData = {
-	    
-	    "ent_fbid": $localStorage.usuarioData.ent_fbid,
-	    "dob" : yyyy+"-"+mm+"-"+dd
-	};
-   // $scope.selected.item();
+  	var postData = {
+  	    
+  	    "ent_fbid": $localStorage.usuarioData.ent_fbid,
+  	    "dob" : yyyy+"-"+mm+"-"+dd
+  	};
+   
     $http.get($localStorage.updatedob,{params: postData}).then(function(resp) {
-
-        $modalInstance.close($scope.dt);
+      $modalInstance.close($scope.dt);
     	$state.go('app.configurations');
-
     });
+    
   };
 
 });
