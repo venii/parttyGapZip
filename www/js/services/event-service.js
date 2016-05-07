d3947f29d1e1bfa7939c0ca493162903c2b5ee2b
veni <viniciusferreirawk@gmail.com>
2016-05-07 15:19:01 -0300

    Comentarios

--- www/js/services/event-service.js
+++ www/js/services/event-service.js
@@ -14,7 +14,9 @@ angular.module('app.event-service', ['starter','app.utils-service','app.registra
     MainService,
     RegistrationService
     ) {
-		
+		  
+
+      //função para pegar  primeira pagina de eventos do facebook pela api do partty
       this.getFirstEvent = function(token,callback){
         var postData = {
                   "sessfb": token
@@ -22,6 +24,7 @@ angular.module('app.event-service', ['starter','app.utils-service','app.registra
         this.getEvents(postData,callback);
       }
 
+      //função para pegar eventos do fb (atraves de paginas de referencias vindo da graph)
       this.getEvents = function(postData,callback){
         
         $http.get(AdressService.geteventsfb,{params: postData})
@@ -32,7 +35,7 @@ angular.module('app.event-service', ['starter','app.utils-service','app.registra
 
       }
 
-
+      //função para validar token da graph api
       this.validateTokenFetchEvents = function(resp){
           if(resp.data == null || resp.data.error){
             return false;    
@@ -40,7 +43,8 @@ angular.module('app.event-service', ['starter','app.utils-service','app.registra
           return true;
               
       }
-
+      
+      //função para verificar se é o fim da lista de eventos (utilizando as refs da graph api e scroll)
       this.isEndList = function(){
         var currentTop = $ionicScrollDelegate.$getByHandle('scroller').getScrollPosition().top;
         var maxTop = $ionicScrollDelegate.$getByHandle('scroller').getScrollView().__maxScrollTop;
--- www/js/services/registration-service.js
+++ www/js/services/registration-service.js
@@ -13,7 +13,8 @@ angular.module('app.registration-service', ['starter','app.utils-service','app.r
     LoginService,
     MainService
     ) {
-		
+
+      //função de retorno da sessao do FB		
     	this.getFBSessJSON = function(callback){
           var  deviceType = MainService.getDeviceType();
         
@@ -22,20 +23,21 @@ angular.module('app.registration-service', ['starter','app.utils-service','app.r
             "sess_fb": LoginService.getToken()
           };
 
-        
           $http.get(AdressService.getfbidbysess,{params: postData}).then(function(resp) {
             callback(resp);
           });
       }
 
+      //função para validar sessao do FB
       this.verifyFBSessJSON = function(resp,callback){
           if(resp.data.error || !resp.data.usuario){
             alert("Autentique com o Facebook novamente.");
-            
+            //caso a sessao seja invalida o sistema invalida tudo e manda para o login sem autenticar      
             LoginService.loggout(callback);
           }
       }
 
+      //função para atualizar caracteristicas do dispositivo mobile
       this.updateDeviceDetailsJSON = function(resp,deviceType){
         var postData = {
             
@@ -44,9 +46,9 @@ angular.module('app.registration-service', ['starter','app.utils-service','app.r
         };
 
         $http.get($localStorage.updatedevicedetails ,{params: postData});
-          
       }
 
+      //função para autenticar no servidor do partty
       this.loginParttyJSON = function(callback,callback_error){
         var postData = this.getUserData();
 
@@ -59,6 +61,7 @@ angular.module('app.registration-service', ['starter','app.utils-service','app.r
 
       }
 
+      //funçao para verificar o json vindo de loginPartty se o usuario é um novo usuario
       this.isNewUser = function(resp,callback){
         if(resp.data.newuser){
           callback();
@@ -73,6 +76,7 @@ angular.module('app.registration-service', ['starter','app.utils-service','app.r
         }
       }
 
+      //função para salvar os dados do usuario
       this.saveUserData = function(resp){
           
           var profilePic = null;
@@ -100,18 +104,22 @@ angular.module('app.registration-service', ['starter','app.utils-service','app.r
           }
       }
 
+      //função para pegar json de dados do usuario
       this.getUserData = function(){
         return $localStorage.usuarioData;
       }
 
+      //função para pegar o ID do FB do usuario
       this.getUserFbID = function(){
         return $localStorage.usuarioData.ent_fbid;
       }
 
+      //função para pegar o usuario do FB do usuario
       this.getUserFbName = function(){
         return $localStorage.usuarioData.ent_first_name;
       }
-
+      
+      //função para trocar a coverPhoto no menu
       this.changeCoverMenuPhoto = function(url){
         try{
           angular.element(document.querySelector('#profilecontentmenu')).html("<img src='"+url+"' style='border-radius: 40px / 20px ;width:64px;height:64px' width='64' heigth='64' />");
