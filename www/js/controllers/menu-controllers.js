189ffb0f207e3231d396cd601b4f5b1948bc90f2
veni <viniciusferreirawk@gmail.com>
2016-05-07 15:59:19 -0300

    Comentarios

--- www/js/controllers/menu-controllers.js
+++ www/js/controllers/menu-controllers.js
@@ -30,54 +30,50 @@ angular.module('app.menu', ['starter'])
   $localStorage.editprofile = $localStorage.restaddress + 'editProfile';
   $localStorage.upload_user_image = $localStorage.restaddress + 'upload_user_image';
   $localStorage.uploadchunk = $localStorage.restaddress + 'uploadChunk';
-  /*
-  $rootScope.$on('$cordovaNetwork:offline', function(event, networkState){
-    
-    if(!$state.is('app.login')){
-      UtilsService.setInternetState(networkState);
-      $state.go('app.login');
-    }
 
-  });*/
-  
+  //gerencia chat , menus superiores (top) e armazena urls da api do partty
+
+  //função para abrir menu esquerdo top  
   $scope.toggleLeftSideMenu = function() {
     MenuService.toggleSideMenu('left');
   };
 
+  //função para abrir menu direito top
   $scope.toggleRightSideMenu = function() {
-      
+      //carrega lista de amigos
       MenuService.toggleSideMenu('right',function(){
         FriendsService.loadFriendList($scope);
         
       });
-    
   };
 
-
+  //função de realizar loggout e enviar para login
   $scope.logout = function() {
     $state.go("app.loggedout");
-    
   };
 
+  //envia para configurações
   $scope.configurations = function() {
     $state.go("app.configurations");
   };
 
+  //envia para events
   $scope.events = function() {
     $state.go("app.events");
   };
 
+  //envia para profile
   $scope.profile = function() {
     $state.go("app.profile");
   };
 
-
-
+  //carrega o chat com a pessoa selecionada
   $scope.loadchat = function(idfb,name,pic){
+      
       delete $rootScope.chatUsrData;
       $rootScope.chatUsrData = {"idfb" : idfb, "name" : name ,"pic" : pic};
-  
       $state.go("app.chat",{"idfb" : idfb});
+
   };
   
 });
