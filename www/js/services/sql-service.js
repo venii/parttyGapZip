27879b1450630f29cb69876db214640e1b601323
Vinicius <viniciusferreirawk@gmail.com>
2016-07-28 13:30:42 -0300

    fix

--- www/js/services/sql-service.js
+++ www/js/services/sql-service.js
@@ -4,19 +4,29 @@ angular.module('app.sql-service', ['starter'])
   	return $resource(HOST_API+'/perfil/:id');
 })
 .service('SQLService',function($q,$localStorage,$ionicLoading,UtilsService,Perfil,GraphService) {
+
+           this.schema = [
+                            { nome: "fb_events", campos: ["id_fb_events", "nome","data_evento"] },
+                            { nome: "fb_events_attending", campos: ["id_fb_events_attending", "id_fb_events","id_fb_attending"] },
+                            { nome: "pt_chat", campos: ["id_pt_chat", "id_fb_sender","id_fb_receiver","msg","data_envio"] },
+
+ 
+                         ];
+
+           
            this.getDB = function() {
            		var deferred = $q.defer();
            		try{
                   
-           		    var db = openDatabase("dbapp_partty.db", "1.0", "dbapp_partty.db", 200000);
+           		  var db = openDatabase("dbapp_partty.db", "1.0", "dbapp_partty.db", 200000);
                 
-                  deferred.resolve(db);
+                deferred.resolve(db);
 
-                }catch(err){
-                    deferred.reject(db);
-                }
+              }catch(err){
+                deferred.reject(db);
+              }
 
-                return deferred.promise;
+              return deferred.promise;
            };
 
 
@@ -27,8 +37,7 @@ angular.module('app.sql-service', ['starter'])
 							             usar campos dos $resources
                     	*/
                     	tx.executeSql('CREATE TABLE IF NOT EXISTS fb_events           (id_fb_events           unique, nome,data_evento);');
-                    	tx.executeSql('CREATE TABLE IF NOT EXISTS fb_events_attending (id_fb_events_attending unique, id_fb_events);');
-                		  tx.executeSql('CREATE TABLE IF NOT EXISTS fb_profiles         (id_fb_profiles         unique, id_fb_attending);');
+                    	tx.executeSql('CREATE TABLE IF NOT EXISTS fb_events_attending (id_fb_events_attending unique, id_fb_events,id_fb_attending);');
                       tx.executeSql('CREATE TABLE IF NOT EXISTS pt_chat             (id_pt_chat             unique, id_fb_sender,id_fb_receiver,data_envio);');
                       tx.executeSql('CREATE TABLE IF NOT EXISTS pt_perfil           (id_pt_perfil           unique, if_fb,nome,sexo,idade,email,foto,fb_token,pt_token);');
                       
@@ -44,6 +53,29 @@ angular.module('app.sql-service', ['starter'])
                     	
                 });
               });
+           }
+
+           this.createTable = function(nome,campos){
+              /*por enquanto a primeira field eh sempre unique (id)*/
+              this.getDB().then(function(db){
+                db.transaction(function(tx) {
+                
+                  tx.executeSql('INSERT INTO foo ('+campos.join(',')+') VALUES (' +')', [id, userValue]);
+                
+                });
+              });
+             }
+
+           }
+
+           this.insertIntoTable = function(){
+            this.getDB().then(function(db){
+              db.transaction(function(tx) {
+              
+                tx.executeSql('INSERT INTO foo (id, text) VALUES (?, ?)', [id, userValue]);
+              
+              });
+            });
            }
 
 
