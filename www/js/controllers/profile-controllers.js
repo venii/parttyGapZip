f6bd48babe1e9a41c0b73bdd45910646b5bd1f60
Vinicius <viniciusferreirawk@gmail.com>
2016-08-30 13:36:59 -0300

    fix upload

--- www/js/controllers/profile-controllers.js
+++ www/js/controllers/profile-controllers.js
@@ -1,6 +1,6 @@
 angular.module('profile.controllers', ['starter'])
 
-.controller('ProfileCtrl', function($scope,$state,$localStorage,$ionicPopup,$timeout,ProfileService,LoginService,Perfil,HOST_API) {
+.controller('ProfileCtrl', function($q,$scope,$state,$localStorage,$ionicPopup,$timeout,ProfileService,LoginService,Perfil,HOST_API) {
       $scope.showSpinner = true;
 
       
@@ -11,8 +11,6 @@ angular.module('profile.controllers', ['starter'])
 
       var fbid = $localStorage.fbid;
       $scope.url_photo_slot = HOST_API+"/uploads/"+fbid+"-";
-
-
 
       var data = {};
       data.id = fbid;
@@ -26,7 +24,46 @@ angular.module('profile.controllers', ['starter'])
 
 
       $scope.changephoto1 = function(){
-        var slot = 1;
+        $scope.uploadFile(1).then(function(uri){
+          $scope.imgPIC1 = uri;
+        });
+      }
+
+      $scope.changephoto2 = function(){
+        $scope.uploadFile(2).then(function(uri){
+          $scope.imgPIC2 = uri;
+        });
+      }
+
+      $scope.changephoto3 = function(){
+        $scope.uploadFile(3).then(function(uri){
+          $scope.imgPIC3 = uri;
+        });
+      }
+
+      $scope.changephoto4 = function(){
+        $scope.uploadFile(4).then(function(uri){
+          $scope.imgPIC4 = uri;
+        });
+      }
+
+      $scope.changephoto5 = function(){
+        $scope.uploadFile(5).then(function(uri){
+          $scope.imgPIC5 = uri;
+        });
+      }
+
+
+
+      $scope.updateProfile = function(){
+
+      }
+
+
+      /*faz upload para a api*/
+      $scope.uploadFile = function(slot){
+        var defer = $q.defer();
+
         ProfileService.returnFoto().then(function(blob){
           ProfileService.uploadPhoto(blob,slot);
             
@@ -39,19 +76,11 @@ angular.module('profile.controllers', ['starter'])
             });
 
             $timeout(function(){
-              $scope.imgPIC1 = $scope.url_photo_slot + slot+".jpg";
+              defer.resolve($scope.url_photo_slot + slot+".jpg");
             },1000);
             
         });
-      }
 
-
-
-      $scope.updateProfile = function(){
-        console.log("##");
-      }
-
-      $scope.uploadFileToApi = function(){
-        
+        return defer.promise;
       }
 });
