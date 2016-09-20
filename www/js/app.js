5dc98c5da3aa0b730fe48a2922d0473faf54e7e1
veni <viniciusferreirawk@gmail.com>
2016-09-20 15:48:01 -0300

    fix newmatches

--- www/js/app.js
+++ www/js/app.js
@@ -76,6 +76,7 @@ angular.module('starter', [	 'ionic',
       'http://localhost.*/**',
       'http://127.0.0.1:8080/**',
       'http://192.168.0.13/**',
+      'https://*.phonegap.com/**'
     ]);
 
     $stateProvider
