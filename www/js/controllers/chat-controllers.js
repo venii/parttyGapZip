566c7c82c344ab66def1b8188f991b3694883a1f
veni <viniciusferreirawk@gmail.com>
2016-09-24 18:45:16 -0300

    fix perfil pop

--- www/js/controllers/chat-controllers.js
+++ www/js/controllers/chat-controllers.js
@@ -1,9 +1,12 @@
 angular.module('chat.controllers', ['starter'])
-.controller('ChatCtrl', function($scope,$stateParams,$localStorage,Perfil,Chat) {
+.controller('ChatCtrl', function($q,$scope,$stateParams,$localStorage,Perfil,Chat) {
     $scope.nomeAmigo   = "";
     $scope.imagemAmigo = "";
+    $scope.imagemMinha = "";
+
     $scope.chat 	   = new Array;
-    
+    $scope.carregaChat = false;
+    //CARREGA PERFIL DE QUEM ESTIVER CONVERSANDO
     var data = {};
     data.id  = $stateParams.idfb;
 
@@ -13,13 +16,32 @@ angular.module('chat.controllers', ['starter'])
 			$scope.imagemAmigo = r.Perfil.imgPIC1;
 		}
 	});
+	//CARREGA MEU PERFIL
+	var data = {};
+    data.id  = $localStorage.fbid;
 
+	Perfil.get(data,function(r){
+		if(r.Perfil){
+			$scope.imagemMinha = r.Perfil.imgPIC1;
+		}
+	});
 
+	//CARREGA CHAT DA PESSOA QUE ESTOU FALANDO
 	var data  = {};
 	data.fbid = $stateParams.idfb;
+	var meusChatP = Chat.get(data).$promise;
+	//CARREGA MEU CHAT
+	var data  = {};
+	data.fbid = $localStorage.fbid;
+	var teusChatP = Chat.get(data).$promise;
 
-	Chat.get(data,function(r){
-		$scope.chat = r.Chat;
+	$q.all([meusChatP,teusChatP]).then(function(r){
+		var chat1 = r[0].Chat.map(function(f){f.eu = false; return f;});
+		var chat2 = r[1].Chat.map(function(f){f.eu = true;  return f;});;
+
+		var chat = chat1.concat(chat2);
+		$scope.chat = chat;
+		$scope.carregaChat = true;
 	});
 
 	$scope.enviarMSG = function(m){
@@ -28,10 +50,12 @@ angular.module('chat.controllers', ['starter'])
 			data.fbid 	    = $localStorage.fbid;
 			data.amigoFB    = $stateParams.idfb;
 			data.data_envio = new Date();
-			
+			data.msg 		= m;
+
 			Chat.save(data,function(r){
 				$scope.chat.push(data);
 				$scope.busca = "";
+				document.querySelector("#textEnvia").value = "";
 			});
 		}
 	}
