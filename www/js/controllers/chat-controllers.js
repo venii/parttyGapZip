85af84a78d0a1c1a3646655b26a957c93011e73c
veni <viniciusferreirawk@gmail.com>
2016-09-24 17:55:14 -0300

    fix perfil pop

--- www/js/controllers/chat-controllers.js
+++ www/js/controllers/chat-controllers.js
@@ -1,5 +1,39 @@
 angular.module('chat.controllers', ['starter'])
-.controller('ChatCtrl', function($scope,$stateParams) {
+.controller('ChatCtrl', function($scope,$stateParams,$localStorage,Perfil,Chat) {
+    $scope.nomeAmigo   = "";
+    $scope.imagemAmigo = "";
+    $scope.chat 	   = new Array;
+    
+    var data = {};
+    data.id  = $stateParams.idfb;
 
+	Perfil.get(data,function(r){
+		if(r.Perfil){
+			$scope.nomeAmigo   = r.Perfil.name;
+			$scope.imagemAmigo = r.Perfil.imgPIC1;
+		}
+	});
+
+
+	var data  = {};
+	data.fbid = $stateParams.idfb;
+
+	Chat.get(data,function(r){
+		$scope.chat = r.Chat;
+	});
+
+	$scope.enviarMSG = function(m){
+		if(m !== undefined && m != ""){
+			var data 	    = {};
+			data.fbid 	    = $localStorage.fbid;
+			data.amigoFB    = $stateParams.idfb;
+			data.data_envio = new Date();
+			
+			Chat.save(data,function(r){
+				$scope.chat.push(data);
+				$scope.busca = "";
+			});
+		}
+	}
     
 });
